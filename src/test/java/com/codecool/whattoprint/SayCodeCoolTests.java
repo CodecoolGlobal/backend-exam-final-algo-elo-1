ddda26f484e383b76b833ef5f76bc8faf096974e
Bendeguz Hajdu <bendeguz.hajdu@hawatconsulting.com>
2021-03-21 21:59:38 +0100

    test fix

--- src/test/java/com/codecool/whattoprint/SayCodeCoolTests.java
+++ src/test/java/com/codecool/whattoprint/SayCodeCoolTests.java
@@ -1,7 +1,9 @@
 package com.codecool.whattoprint;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SayCodeCoolTests {
     private final static String[] words = new String[]{"Code", "Cool", "Apple"};
